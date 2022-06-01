1ef9ddbfe2e6a35c5510ac826713e977076535ea
Tzukuyomii <97518242+Tzukuyomii@users.noreply.github.com>
2022-06-01 18:36:57 +0200

    Fix landing page e titolo preferred

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,11 @@
-import { FooterShared } from '../components/FooterShared';
-import { HeaderShared } from '../components/HeaderShared';
-import { CardShared } from '../components/CardShared';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { Details } from '../components/Details';
-import notFound from '../assets/not-found.svg';
-import PreferredShared from '../components/PreferredShared';
+import { FooterShared } from "../components/FooterShared";
+import { HeaderShared } from "../components/HeaderShared";
+import { CardShared } from "../components/CardShared";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Details } from "../components/Details";
+import notFound from "../assets/not-found.svg";
+import PreferredShared from "../components/PreferredShared";
 
 export function Homepage() {
   const [animals, setAnimals] = useState([]);
@@ -15,10 +15,10 @@ export function Homepage() {
   const [preferredAnimals, setPreferredAnimals] = useState([]);
   const [searchedAnimals, setSearchedAnimals] = useState([]);
   const [input, setInput] = useState();
-  const [selection, setSelection] = useState('name');
+  const [selection, setSelection] = useState("name");
 
   useEffect(() => {
-    fetch('https://zoo-animal-api.herokuapp.com/animals/rand/8')
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand/8")
       .then((response) => response.json())
       .then((data) => setAnimals(data));
   }, []);
@@ -61,7 +61,7 @@ export function Homepage() {
 
   useEffect(() => {
     const pippo = animals.filter((animal) =>
-      selection === 'name'
+      selection === "name"
         ? animal.name.toLowerCase().includes(input)
         : animal.geo_range.toLowerCase().includes(input)
     );
@@ -69,45 +69,45 @@ export function Homepage() {
   }, [input, animals, selection]);
 
   return (
-    <div className='fredoka'>
+    <div className="fredoka">
       <HeaderShared
         openPreferred={openPreferred}
         preferredAnimals={preferredAnimals}
       />
-      <div className='h-100 background-home padding-bottom'>
-        <div className='d-flex bg-feed shadow'>
-          <h1 className='py-3 px-4 font-color'>Feed</h1>
-          <div className='d-flex gap-4 align-items-center'>
+      <div className="h-100 background-home padding-bottom">
+        <div className="d-flex bg-feed shadow">
+          <h1 className="py-3 px-4 font-color">Feed</h1>
+          <div className="d-flex gap-4 align-items-center">
             <input
-              className='rounded-pill py-2 ps-4 pe-5 border-0  button-dark  fs-5'
-              placeholder='Search animals'
+              className="rounded-pill py-2 ps-4 pe-5 border-0  button-dark  fs-5"
+              placeholder="Search animals"
               onChange={handleInput}
             />
-            <div className='d-flex gap-2 fs-4' onChange={handleSelection}>
+            <div className="d-flex gap-2 fs-4" onChange={handleSelection}>
               Filter by:
               <label>
                 <input
-                  className='me-2'
-                  name='filter'
-                  type='radio'
-                  value='name'
+                  className="me-2"
+                  name="filter"
+                  type="radio"
+                  value="name"
                   defaultChecked
                 />
                 <span>Name</span>
               </label>
               <label>
                 <input
-                  className='me-2'
-                  name='filter'
-                  type='radio'
-                  value='geo_range'
+                  className="me-2"
+                  name="filter"
+                  type="radio"
+                  value="geo_range"
                 />
                 <span>Geo range</span>
               </label>
             </div>
           </div>
         </div>
-        <div className='d-flex max-width gap-4 m-auto justify-content-evenly align-items-around flex-wrap pt-5'>
+        <div className="d-flex max-width gap-4 m-auto justify-content-evenly align-items-around flex-wrap pt-5">
           {!input &&
             animals.map((animal) => (
               <li key={animal.id}>
@@ -123,7 +123,7 @@ export function Homepage() {
             ))}
 
           {input && searchedAnimals.length < 1 && (
-            <img src={notFound} alt='Not found' />
+            <img src={notFound} alt="Not found" />
           )}
         </div>
       </div>
